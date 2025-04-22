cadf90205b072d8f2553201c115a614eff8e692f
EmmanuelOlanipekun <ewale32@gmail.com>
2025-04-22 01:06:47 +0100

    Initial commit - added base files

---
+++ src/middleware/db.js
@@ -0,0 +1,14 @@
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+
+const connectionURL = 'mongodb://localhost:27017'
+const databaseName = 'task-manager'
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+    if(error) {
+        return console.log('Unable to connecg to database!')
+    }
+
+    const db = client.db(databaseName)
+    
+})
