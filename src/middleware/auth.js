0e50abf8eb4230933dec618deafe408d06b56a52
EmmanuelOlanipekun <ewale32@gmail.com>
2025-08-12 00:55:58 +0100

    Few changes

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,7 +5,7 @@ const user = require('../models/user')
 const auth = async (req, res, next) => {
   try {
     const token =req.header('Authorization').replace('Bearer ', '')
-    const decoded = jwt.verify(token, 'thisismynewcourse')
+    const decoded = jwt.verify(token)
     const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
 
     if(!user) {
